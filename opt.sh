2b154a734a543dd77d83ee1faf3af959516191f7
Joao Costa <joaopccosta@gmail.com>
2022-05-06 12:56:18 +0100

    Added long-press special characters enabled.

--- opt.sh
+++ opt.sh
@@ -4,7 +4,10 @@ PI_USR=$2
 
 cp files/.ssh/config ~/.ssh/config
 sed -i config.bak "s/PIHOLE_IP_ADDR/$PIHOLE_IP_ADDR/g; s/PI_USR/$PI_USR/g" ~/.ssh/config
-echo "Copied ~/.ssh/config"
+echo "[OK] Copied ~/.ssh/config."
+
+defaults write -g ApplePressAndHoldEnabled -bool true
+echo "[OK] Enabled long-press on keys for special characters."
 
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
